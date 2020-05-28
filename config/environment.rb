54c56e26db459e044604dad6697767e04c87b45d
Alex Kln <kalininmalinin@gmail.com>
2020-05-29 02:58:09 +0300

    add event delete, edit alerts, i18n plur, disable div error

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Disable div.field_with_error
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  html_tag.html_safe
+end
