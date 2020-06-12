7e19d79fc109eb03bb43931243ae0e49b2ae439a
Alex Kln <kalininmalinin@gmail.com>
2020-06-12 21:03:36 +0300

    add mail notification for comments and subscriptions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,6 +8,7 @@ class CommentsController < ApplicationController
     @new_comment.user = current_user
 
     if @new_comment.save
+      notify_subscribers(@event, @new_comment)
       redirect_to @event, notice: I18n.t('controllers.comments.created')
     else
       render 'events/show', alert: I18n.t('controllers.comments.error')
@@ -28,6 +29,18 @@ class CommentsController < ApplicationController
   end
 
   private
+
+  def notify_subscribers
+    # собираем всех подписчиков и автора события в массив мэйлов, исключаем повторяющиеся
+    all_emails = (event.subscriptions.map(&:user_email) + [event.user.email]).uniq
+
+    # XXX: Этот метод может выполняться долго из-за большого числа подписчиков
+    # поэтому в реальных приложениях такие вещи надо выносить в background задачи!
+    all_emails.each do |mail|
+      EventMailer.comment(event, comment, mail).deliver_now
+    end
+  end
+
   def set_event
     @event = Event.find(params[:event_id])
   end
