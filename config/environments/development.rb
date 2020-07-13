69490178892f38b9bfbb352edfc39a695869c94c
Alex Kln <kalininmalinin@gmail.com>
2020-07-13 21:31:41 +0300

    edit mail configs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,29 +62,6 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
-  # config.action_mailer.delivery_method = :letter_opener
-  config.action_mailer.delivery_method = :mailjet
+  config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
-
-  # config.action_mailer.delivery_method = :smtp
-  # отправка почты через mailjet
-  # ActionMailer::Base.smtp_settings = {
-  #   address:              'in-v3.mailjet.com',
-  #   port:                 '587',
-  #   user_name:            ENV['MAILJET_API_KEY'],
-  #   password:             ENV['MAILJET_SECRET_KEY'],
-  #   enable_starttls_auto: true,
-  #   authentication: 'plain'
-  # }
-  # отправка почты по протоколу SMTP
-  # config.action_mailer.delivery_method = :smtp
-  # Настройки для работы через GMail аккаунт
-  # config.action_mailer.smtp_settings = {
-  #   address: 'smtp.gmail.com',
-  #   port: '587',
-  #   user_name: '#', # не используйте для тестов свои реальные ящики
-  #   password: Rails.application.credentials.mailer[:password],
-  #   authentication: 'plain',
-  #   enable_starttls_auto: true
-  # }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,34 +97,6 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   # отправка почты через mailjet
   config.action_mailer.delivery_method = :mailjet
-
-  # отправка почты по протоколу SMTP
-  # config.action_mailer.delivery_method = :smtp
-  # ActionMailer::Base.smtp_settings = {
-  #   address:              'in-v3.mailjet.com',
-  #   port:                 '587',
-  #   user_name:            ENV['MAILJET_API_KEY'],
-  #   password:             ENV['MAILJET_SECRET_KEY'],
-  #   enable_starttls_auto: true,
-
-    # Настройки для Sendgrid
-    # :address        => 'smtp.sendgrid.net',
-    # :port           => '587',
-    # :authentication => :plain,
-    # :user_name      => ENV['SENDGRID_USERNAME'],
-    # :password       => ENV['SENDGRID_PASSWORD'],
-    # :domain         => 'heroku.com',
-    # :enable_starttls_auto => true
-
-    # Настройки для Mailgun
-    # port:            ENV['MAILGUN_SMTP_PORT'],
-    # address:         ENV['MAILGUN_SMTP_SERVER'],
-    # user_name:       ENV['MAILGUN_SMTP_LOGIN'],
-    # password:        ENV['MAILGUN_SMTP_PASSWORD'],
-    # domain:          'heroku.com',
-    # authentication:  :plain,
-  # }
-
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
