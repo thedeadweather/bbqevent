392ae6058dc5105d6c3d620c45e33345bbe92d0a
Alex Kln <kalininmalinin@gmail.com>
2020-09-28 17:57:47 +0300

    small edit of notification in comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -22,7 +22,7 @@ class CommentsController < ApplicationController
     if current_user_can_edit?(@comment)
       @comment.destroy!
     else
-      msessage = { alert: I18n.t('controllers.comments.error') }
+      message = { alert: I18n.t('controllers.comments.error') }
     end
 
     redirect_to @event, message
